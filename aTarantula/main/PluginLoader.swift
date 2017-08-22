28c60cf83d69cd8a8bbe2d49cfe478cee41b7410
Ilya Mikhaltsou <morpheby@gmail.com>
2017-08-21 19:06:41 -0700

    Tested and fixed test plugin

---
+++ aTarantula/main/PluginLoader.swift
@@ -0,0 +1,24 @@
+//
+//  PluginLoader.swift
+//  aTarantula
+//
+//  Created by Ilya Mikhaltsou on 8/20/17.
+//  Copyright © 2017 morpheby. All rights reserved.
+//
+
+import Foundation
+import TarantulaPluginCore
+
+class PluginLoader {
+    func listAllPluginPaths() -> [String] {
+        return []
+    }
+
+    func loadPlugin(fromPath path: String) {
+
+    }
+
+    var exporters: [Any] = []
+    var crawlers: [CrawlingPluginProtocol] = []
+
+}
