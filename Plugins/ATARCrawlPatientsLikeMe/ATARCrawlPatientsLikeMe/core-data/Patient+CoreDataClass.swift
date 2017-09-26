cfe382d23157caac4c1c62ee3aa6b41c78af51f9
Ilya Mikhaltsou <morpheby@gmail.com>
2017-09-26 06:22:17 -0700

    Implemented crawlDrugPatients

---
+++ Plugins/ATARCrawlPatientsLikeMe/ATARCrawlPatientsLikeMe/core-data/Patient+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Patient+CoreDataClass.swift
+//  ATARCrawlExampleWebsite
+//
+//  Created by Ilya Mikhaltsou on 9/26/17.
+//  Copyright © 2017 morpheby. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Patient)
+public class Patient: Crawlable {
+
+}
--- Plugins/ATARCrawlPatientsLikeMe/ATARCrawlPatientsLikeMe/crawling/crawlDrugPatients.swift
+++ Plugins/ATARCrawlPatientsLikeMe/ATARCrawlPatientsLikeMe/crawling/crawlDrugPatients.swift
@@ -21,13 +21,93 @@ func crawlDrugPatients(_ object: DrugPatients, usingRepository repo: Repository,
         return
     }
 
-    let data = try plugin.networkManager?.stringData(url: objectUrl) ?? String(contentsOf: objectUrl)
-
     var relatedCrawlables: [CrawlableObject] = []
 
-    guard let html = Kanna.HTML(html: data, encoding: .utf8) else {
-        throw CrawlError(url: objectUrl, info: "Unable to parse HTML")
+    var patientsAll: [Patient] = []
+    var pageNum: Int = 1
+    var allPages: [Int]? = nil
+    var pageCount: Int? = nil
+
+    repeat {
+        guard var objectUrlComponents = URLComponents(url: objectUrl, resolvingAgainstBaseURL: true) else {
+            fatalError("Unable to decompose URL \(objectUrl)")
+        }
+        if pageNum > 1 {
+            if objectUrlComponents.queryItems == nil { objectUrlComponents.queryItems = [] }
+            objectUrlComponents.queryItems?.append(URLQueryItem(name: "page", value: "\(pageNum)"))
+        }
+
+        guard let actualUrl = pageNum > 1 ? objectUrlComponents.url : objectUrl else {
+            fatalError("Unable to reconstruct URL \(objectUrlComponents)")
+        }
+        let data = try plugin.networkManager?.stringData(url: actualUrl) ?? String(contentsOf: actualUrl)
+
+        guard let html = Kanna.HTML(html: data, encoding: .utf8) else {
+            throw CrawlError(url: objectUrl, info: "Unable to parse HTML")
+        }
+
+        let patients: [Patient] = {
+            html.xpath("//div[@id='patient-search-table']//div[@class='tertiary-title']/a/@href").flatMap { element in
+                guard let urlString = element.text,
+                    let url = URL(string: urlString, relativeTo: plugin.baseUrl) else { return nil }
+
+                let relatedObject = repo.performAndWait {
+                    repo.readAllObjects(Patient.self, withSelection: .object(url: url)).first ??
+                        repo.newObject(forUrl: url, type: Patient.self)
+                }
+                relatedCrawlables.append(relatedObject)
+                return relatedObject
+            }
+        }() .flatMap { x in x }
+
+        patientsAll.append(contentsOf: patients)
+
+        if allPages == nil {
+            let pages: [Int] = {
+                html.xpath("//div[contains(@class, 'paging-ui')]//div[@class='pagination']/a[position() > 1 and position() < last()]").flatMap { element in
+                    guard let pageNumStr = element.text,
+                    let pageNum = Int(pageNumStr) else { return nil }
+
+                    return pageNum
+                }
+            }() .flatMap { x in x }
+
+            precondition({
+                for (prevPage, page) in zip([1] + pages.dropLast(), pages) {
+                    if prevPage + 1 != page {
+                        return false
+                    }
+                }
+                return true
+            }(), "Missing pages in paginator page list: \(pages)")
+
+            allPages = pages
+
+            if let lastPage = pages.last {
+                pageCount = lastPage
+            } else {
+                pageCount = 1
+            }
+        }
+
+        pageNum += 1
+    } while pageNum <= pageCount!
+
+    // Store object
+    repo.perform {
+        // We don't have one single HTML document for the multi-page data,
+        // so we just set it to nil
+        object.originalHtml = nil
+
+        object.patients = Set(patientsAll) as NSSet
+
+        object.objectIsCrawled = true
+
+        if needsToBeFiltered(object: object, method: plugin.filterMethod) {
+            object.filter(newObjects: relatedCrawlables)
+        } else {
+            object.unfilter(newObjects: relatedCrawlables)
+        }
+
     }
-
-
 }
